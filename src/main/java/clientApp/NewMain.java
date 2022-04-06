7c7559fe3b3fef85933ac3d2fd23c91653fddbcc
Witcher <[200~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~>
2022-04-06 14:00:20 +0300

    запервая из раздела три

---
+++ src/main/java/clientApp/NewMain.java
@@ -0,0 +1,29 @@
+package clientApp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class NewMain {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8181;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new
+                     PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+
+            out.println("Get /HTTP/1.1\n" +
+                    "Host:netology.ru\n\n\n");
+
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
